ddc13a1751748f25220c8a64f92dd885cd4f8bd7
Gurpreet Singh <gpsingh02414@gmail.com>
2024-02-04 02:07:31 +0530

    commit

---
+++ src/App.test.js
@@ -0,0 +1,47 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import FeedbackForm from "./FeedbackForm";
+
+describe("Feedback Form", () => {
+  test("User is able to submit the form if the score is lower than 5 and additional feedback is provided", () => {
+    const score = "3";
+    const comment = "The pizza crust was too thick";
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    // You have to write the rest of the test below to make the assertion pass
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: score } });
+
+    const rangeC = screen.getByLabelText(/Comments:/);
+    fireEvent.change(rangeC, { target: { value: comment } });
+    
+
+    const submit = screen.getByRole("button");
+    fireEvent.click(submit);
+
+    
+    
+
+expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment,
+    });
+  });
+
+  test("User is able to submit the form if the score is higher than 5, without additional feedback", () => {
+    const score = "9";
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit} />);
+
+    // You have to write the rest of the test below to make the assertion pass
+  const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: score } });
+
+    const submit = screen.getByRole("button");
+    fireEvent.click(submit);
+    expect(handleSubmit).toHaveBeenCalledWith({
+      score,
+      comment: "",
+    });
+  });
+});
